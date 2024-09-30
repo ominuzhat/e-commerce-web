50410cbc5135f0f5381fa3ab472bc1b7ecc877bd
omi hasan <omihasan1307@gmail.com>
2024-09-30 22:28:02 +0600

    refetch wishlist done

--- src/utils/actions/get/get.action.ts
+++ src/utils/actions/get/get.action.ts
@@ -1,5 +1,8 @@
 "use server";
 import axiosInstance from "@/lib/AxiosInstance";
+import envConfig from "@/lib/config/envConfig";
+import axios from "axios";
+import { cookies } from "next/headers";
 
 export const getSingleWishList = async (id: number) => {
   try {
@@ -15,10 +18,27 @@ export const getSingleWishList = async (id: number) => {
 };
 
 export const getWishList = async () => {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const fetchOptions = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    next: {
+      tags: ["wishlist"],
+    },
+  };
+
   try {
-    const response = await axiosInstance.get(`/wishlist`);
-    return response;
+    const response = await fetch(`${envConfig.baseApi}/wishlist`, fetchOptions);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch wishlist");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
   } catch (error: any) {
-    throw new Error(error);
+    throw new Error(error.message || "An unknown error occurred");
   }
 };
--- src/utils/actions/post/post.action.ts
+++ src/utils/actions/post/post.action.ts
@@ -1,5 +1,6 @@
 "use server";
 import axiosInstance from "@/lib/AxiosInstance";
+import { revalidateTag } from "next/cache";
 
 export const addToCart = async (data: any) => {
   try {
@@ -15,6 +16,9 @@ export const addToCart = async (data: any) => {
 export const addWishlist = async (data: any) => {
   try {
     const res = await axiosInstance.post("/wishlist", data);
+
+    revalidateTag("wishlist");
+
     return res?.data;
   } catch (error: any) {
     throw new Error(
